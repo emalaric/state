6d4e13438235803f89dd69fc5729fc40bb80911d
Ernest Malaric <malaric.ernest@gmail.com>
2023-09-14 20:09:15 +0200

    Use state hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,30 @@
 import './App.css'
-import { Component } from 'react'
 import { UserClass, UserFunction, UserChildren } from './user'
+import { useState } from 'react'
 
-const users = [
-  {name: 'Ivan', years: 30},
-  {name: 'Marko', years: 35},
-  {name: 'Ana', years: 25}
-]
 
-export default class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      korisnici: [
-        {name: 'Ivan', years: 30},
-        {name: 'Marko', years: 35},
-        {name: 'Ana', years: 25}
-      ],
-      naziv: "Proizvoljna varijabla"
-    }
+export default function App() {
+
+  const [korisnici, setKorisnici] = useState([
+    {name: 'Ivan', years: 30},
+    {name: 'Marko', years: 35},
+    {name: 'Ana', years: 25}
+  ])
+  const [naziv, setNaziv] = useState("Proizvoljna varijabla")
+
+  const promjeniGodine = () =>{
+      setKorisnici(korisnici.map((korisnik) => {
+            return {...korisnik, years: korisnik.years + 1}
+          }))
   }
 
-  promjeniGodine = () =>{
-    const noviKorisnici = this.state.korisnici.map( (korisnik) => {
-      return {...korisnik, years: korisnik.years + 1}
-    }) 
-
-    this.setState({korisnici: noviKorisnici})
-  }
-
-  render() {
-    const { korisnici, naziv} = this.state
-    return (
-      <>
-        <h1>State</h1>
-        <UserClass name={korisnici[0].name} years={korisnici[0].years}/>
-        <UserFunction name={korisnici[1].name} years={korisnici[1].years}/>
-        <UserChildren name={korisnici[2].name} years={korisnici[2].years}><p>{naziv}</p></UserChildren>
-        <button onClick={this.promjeniGodine}>Promjeni godine</button>
-      </>
-    )
-  }
-  
+  return (
+    <>
+      <h1>State</h1>
+      <UserClass name={korisnici[0].name} years={korisnici[0].years}/>
+      <UserFunction name={korisnici[1].name} years={korisnici[1].years}/>
+      <UserChildren name={korisnici[2].name} years={korisnici[2].years}><p>{naziv}</p></UserChildren>
+      <button onClick={promjeniGodine}>Promjeni godine</button>
+    </>
+  )
 }
