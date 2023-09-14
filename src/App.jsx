fa5786470ee4bb8ee1f641a5f7c14e2f632cd0f1
Ernest Malaric <malaric.ernest@gmail.com>
2023-09-14 17:46:31 +0200

    Add state

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,38 @@ const users = [
   {name: 'Ana', years: 25}
 ]
 
-class App extends Component {
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      korisnici: [
+        {name: 'Ivan', years: 30},
+        {name: 'Marko', years: 35},
+        {name: 'Ana', years: 25}
+      ],
+      naziv: "Proizvoljna varijabla"
+    }
+  }
+
+  promjeniGodine = () =>{
+    const noviKorisnici = this.state.korisnici.map( (korisnik) => {
+      return {...korisnik, years: korisnik.years + 1}
+    }) 
+
+    this.setState({korisnici: noviKorisnici})
+  }
+
   render() {
+    const { korisnici, naziv} = this.state
     return (
       <>
         <h1>State</h1>
-        <UserClass name={users[0].name} years={users[0].years}/>
-        <UserFunction name={users[1].name} years={users[1].years}/>
-        <UserChildren name={users[2].name} years={users[2].years}> bla bla bla</UserChildren>
+        <UserClass name={korisnici[0].name} years={korisnici[0].years}/>
+        <UserFunction name={korisnici[1].name} years={korisnici[1].years}/>
+        <UserChildren name={korisnici[2].name} years={korisnici[2].years}><p>{naziv}</p></UserChildren>
+        <button onClick={this.promjeniGodine}>Promjeni godine</button>
       </>
     )
   }
   
 }
-
-export default App
